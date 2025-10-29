8bc9d7444948cda3c7c24b1bfb6b0cd74b6e222e
Pirinka Kondeva <167126915+pepakondeva@users.noreply.github.com>
2025-10-29 22:49:16 +0200

    conf

--- towns.js
+++ towns.js
@@ -3,8 +3,13 @@ $(document).ready(function () {
 });
 // add town
 function addTown() {
+<<<<<<< HEAD
   let TtownName = $("#townNameForAdd").val();
   $("#townNameForAdd").val("");
+=======
+  let townName = $("#townNameForAdd").val();
+  $("#townNameForAdd").val(".......");
+>>>>>>> e15b455 (conf)
   $("#towns").append($("<option>").text(townName));
   $("#result").text(townName + " added.");
 }
